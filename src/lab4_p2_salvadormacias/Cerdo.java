c5d1f51d6f3381f0cf320880294ac3ac97b9e659
papilon10 <salvigna17@gmail.com>
2025-02-14 13:57:06 -0600

    clase cerdo creada

--- src/lab4_p2_salvadormacias/Cerdo.java
+++ src/lab4_p2_salvadormacias/Cerdo.java
@@ -8,6 +8,60 @@ package lab4_p2_salvadormacias;
  *
  * @author Apple
  */
-public class Cerdo {
-    
+public class Cerdo extends Animal {
+
+    public int capacidadBusqueda;
+    public int resitencia;
+
+    public Cerdo() {
+    }
+
+    public Cerdo(int capacidadBusqueda, int resitencia, String nombre, double precio) {
+        super(nombre, precio);
+        this.capacidadBusqueda = capacidadBusqueda;
+        this.resitencia = resitencia;
+    }
+
+    public int getCapacidadBusqueda() {
+        return capacidadBusqueda;
+    }
+
+    public void setCapacidadBusqueda(int capacidadBusqueda) {
+        this.capacidadBusqueda = capacidadBusqueda;
+    }
+
+    public int getResitencia() {
+        return resitencia;
+    }
+
+    public void setResitencia(int resitencia) {
+        this.resitencia = resitencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public void recolectarProductos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Cerdo{" + "capacidadBusqueda=" + capacidadBusqueda + ", resitencia=" + resitencia + '}';
+    }
+
 }
