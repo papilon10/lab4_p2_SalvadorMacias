cc0f73e3152eb5fbc27d48f6caf64df27b173638
papilon10 <salvigna17@gmail.com>
2025-02-14 13:20:39 -0600

    inicio de proyecto

---
+++ src/lab4_p2_salvadormacias/Lab4_p2_SalvadorMacias.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab4_p2_salvadormacias;
+
+/**
+ *
+ * @author Apple
+ */
+public class Lab4_p2_SalvadorMacias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
