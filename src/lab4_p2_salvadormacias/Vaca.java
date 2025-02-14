f9fcf3bdd8c9bd347a4cb0ee26147c0efb1eed96
papilon10 <salvigna17@gmail.com>
2025-02-14 14:01:13 -0600

    clase vaca creada

--- src/lab4_p2_salvadormacias/Vaca.java
+++ src/lab4_p2_salvadormacias/Vaca.java
@@ -8,6 +8,60 @@ package lab4_p2_salvadormacias;
  *
  * @author Apple
  */
-public class Vaca {
-    
+public class Vaca extends Animal {
+
+    public int produccionLeche;
+    public String temperamento;
+
+    public Vaca() {
+    }
+
+    public Vaca(int produccionLeche, String temperamento, String nombre, double precio) {
+        super(nombre, precio);
+        this.produccionLeche = produccionLeche;
+        this.temperamento = temperamento;
+    }
+
+    public int getProduccionLeche() {
+        return produccionLeche;
+    }
+
+    public void setProduccionLeche(int produccionLeche) {
+        this.produccionLeche = produccionLeche;
+    }
+
+    public String getTemperamento() {
+        return temperamento;
+    }
+
+    public void setTemperamento(String temperamento) {
+        this.temperamento = temperamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public void recolectarProductos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ "Vaca{" + "produccionLeche=" + produccionLeche + ", temperamento=" + temperamento + '}';
+    }
+
 }
