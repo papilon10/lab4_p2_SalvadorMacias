a092389c3cc99946185dfdbbed8cc933c44f998a
papilon10 <salvigna17@gmail.com>
2025-02-14 13:52:38 -0600

    clases vacias creadas

---
+++ src/lab4_p2_salvadormacias/Cerdo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4_p2_salvadormacias;
+
+/**
+ *
+ * @author Apple
+ */
+public class Cerdo {
+    
+}
