96f0d1d98deb1b376aa44c6219b7506dcaf145ac
papilon10 <salvigna17@gmail.com>
2025-02-14 13:55:09 -0600

    clase animal creada

--- src/lab4_p2_salvadormacias/Animal.java
+++ src/lab4_p2_salvadormacias/Animal.java
@@ -8,6 +8,40 @@ package lab4_p2_salvadormacias;
  *
  * @author Apple
  */
-public class Animal {
-    
+public abstract class Animal {
+
+    public String nombre;
+    public double precio;
+
+    public Animal() {
+    }
+
+    public Animal(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public abstract void recolectarProductos();
+
+    @Override
+    public String toString() {
+        return "Animal{" + "nombre=" + nombre + ", precio=" + precio + '}';
+    }
+
 }
